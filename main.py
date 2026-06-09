f41ec118865895f3da7797bbbf4e62696c29e3ea
wulfenjarl <yarg007@mac.com>
2026-06-09 11:39:43 -0600

    player shape woo hoo

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
 from constants import SCREEN_HEIGHT
 from constants import SCREEN_WIDTH 
+from constants import PLAYER_RADIUS
+from constants import LINE_WIDTH
 from logger import log_state
+from circleshape import CircleShape
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -11,14 +15,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        
+
 if __name__ == "__main__":
     main()
